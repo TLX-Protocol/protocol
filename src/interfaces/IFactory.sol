9cbff2ef5f56dd6a39160785921717439841ca6b
chase-manning <chase@manning.dev>
2023-06-16 11:15:15 +0100

    create factory contract

---
+++ src/interfaces/IFactory.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.13;
+
+interface IFactory {
+    error TokenExists();
+    error ZeroAddress();
+    error ZeroLeverage();
+    error MaxLeverage();
+
+    event NewLeveragedToken(address indexed token);
+
+    // Creates a new Long and Short Leveraged Token for the given target asset and leverage
+    function createLeveragedTokens(
+        address target,
+        uint256 targetLeverage
+    ) external returns (address longToken, address shortToken);
+
+    // Returns all Leveraged Tokens
+    function allTokens() external view returns (address[] memory);
+
+    // Returns all Long Leveraged Tokens
+    function longTokens() external view returns (address[] memory);
+
+    // Returns all Short Leveraged Tokens
+    function shortTokens() external view returns (address[] memory);
+
+    // Returns the Leveraged Token for the given target asset and leverage
+    function getToken(
+        address target,
+        uint256 targetLeverage,
+        bool isLong
+    ) external view returns (address);
+
+    // Returns true if the Leveraged Token for the given target asset and leverage exists
+    function tokenExists(
+        address target,
+        uint256 targetLeverage,
+        bool isLong
+    ) external view returns (bool);
+
+    // Returns the Leveraged Tokens inverse pair (e.g. ETH3L -> ETH3S)
+    function pair(address token) external view returns (address);
+}
