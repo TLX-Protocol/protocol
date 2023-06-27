70f3f1c3b0d67b1873e8136dc5ee1bc914a52b66
chase-manning <chase@manning.dev>
2023-06-27 11:25:22 +0100

    add gmx to tokens

--- src/libraries/Tokens.sol
+++ src/libraries/Tokens.sol
@@ -4,4 +4,5 @@ pragma solidity ^0.8.13;
 library Tokens {
     address public constant UNI = 0xFa7F8980b0f1E64A2062791cc3b0871572f1F7f0;
     address public constant USDC = 0xaf88d065e77c8cC2239327C5EDb3A432268e5831;
+    address public constant GMX = 0xfc5A1A6EB076a2C7aD06eD22C90d7E710E35ad0a;
 }
