0335f6abb8ed34e39972fdd7640777264d7c32be
chase-manning <chase@manning.dev>
2023-06-29 15:50:59 +0100

    rename leveraged token factory test

--- test/LeveragedTokenFactory.t.sol
+++ test/LeveragedTokenFactory.t.sol
@@ -3,22 +3,16 @@ pragma solidity ^0.8.13;
 
 import {IntegrationTest} from "./shared/IntegrationTest.sol";
 
-import {LeveragedTokenFactory} from "../src/LeveragedTokenFactory.sol";
-
 import {ILeveragedTokenFactory} from "../src/interfaces/ILeveragedTokenFactory.sol";
 import {Tokens} from "../src/libraries/Tokens.sol";
 import {ILeveragedToken} from "../src/interfaces/ILeveragedToken.sol";
 
-contract LeveragedTokenFactoryTest is IntegrationTest {
-    LeveragedTokenFactory public factory;
-
-    function setUp() public {
-        factory = new LeveragedTokenFactory();
-    }
-
+contract LeveragedTokenleveragedTokenFactoryTest is IntegrationTest {
     function testDeployTokens() public {
-        (address longTokenAddress_, address shortTokenAddress_) = factory
-            .createLeveragedTokens(Tokens.UNI, 123);
+        (
+            address longTokenAddress_,
+            address shortTokenAddress_
+        ) = leveragedTokenFactory.createLeveragedTokens(Tokens.UNI, 123);
         ILeveragedToken longToken = ILeveragedToken(longTokenAddress_);
         ILeveragedToken shortToken = ILeveragedToken(shortTokenAddress_);
         assertEq(longToken.name(), "UNI 1.23x Long");
@@ -33,48 +27,70 @@ contract LeveragedTokenFactoryTest is IntegrationTest {
         assertEq(shortToken.targetAsset(), Tokens.UNI);
         assertEq(shortToken.targetLeverage(), 123);
         assertFalse(shortToken.isLong());
-        address[] memory tokens = factory.allTokens();
+        address[] memory tokens = leveragedTokenFactory.allTokens();
         assertEq(tokens.length, 2);
         assertEq(tokens[0], longTokenAddress_);
         assertEq(tokens[1], shortTokenAddress_);
-        address[] memory longTokens = factory.longTokens();
+        address[] memory longTokens = leveragedTokenFactory.longTokens();
         assertEq(longTokens.length, 1);
         assertEq(longTokens[0], longTokenAddress_);
-        address[] memory shortTokens = factory.shortTokens();
+        address[] memory shortTokens = leveragedTokenFactory.shortTokens();
         assertEq(shortTokens.length, 1);
         assertEq(shortTokens[0], shortTokenAddress_);
-        address[] memory allTargetTokens = factory.allTokens(Tokens.UNI);
+        address[] memory allTargetTokens = leveragedTokenFactory.allTokens(
+            Tokens.UNI
+        );
         assertEq(allTargetTokens.length, 2);
         assertEq(allTargetTokens[0], longTokenAddress_);
         assertEq(allTargetTokens[1], shortTokenAddress_);
-        address[] memory emptyTokens = factory.allTokens(Tokens.USDC);
+        address[] memory emptyTokens = leveragedTokenFactory.allTokens(
+            Tokens.USDC
+        );
         assertEq(emptyTokens.length, 0);
-        address[] memory longTargetTokens = factory.longTokens(Tokens.UNI);
+        address[] memory longTargetTokens = leveragedTokenFactory.longTokens(
+            Tokens.UNI
+        );
         assertEq(longTargetTokens.length, 1);
         assertEq(longTargetTokens[0], longTokenAddress_);
-        address[] memory shortTargetTokens = factory.shortTokens(Tokens.UNI);
+        address[] memory shortTargetTokens = leveragedTokenFactory.shortTokens(
+            Tokens.UNI
+        );
         assertEq(shortTargetTokens.length, 1);
         assertEq(shortTargetTokens[0], shortTokenAddress_);
-        address longTokenAddress = factory.token(Tokens.UNI, 123, true);
+        address longTokenAddress = leveragedTokenFactory.token(
+            Tokens.UNI,
+            123,
+            true
+        );
         assertEq(longTokenAddress, longTokenAddress_);
-        address shortTokenAddress = factory.token(Tokens.UNI, 123, false);
+        address shortTokenAddress = leveragedTokenFactory.token(
+            Tokens.UNI,
+            123,
+            false
+        );
         assertEq(shortTokenAddress, shortTokenAddress_);
-        assertTrue(factory.tokenExists(Tokens.UNI, 123, true));
-        assertTrue(factory.tokenExists(Tokens.UNI, 123, false));
-        assertEq(factory.pair(longTokenAddress_), shortTokenAddress_);
-        assertEq(factory.pair(shortTokenAddress_), longTokenAddress_);
+        assertTrue(leveragedTokenFactory.tokenExists(Tokens.UNI, 123, true));
+        assertTrue(leveragedTokenFactory.tokenExists(Tokens.UNI, 123, false));
+        assertEq(
+            leveragedTokenFactory.pair(longTokenAddress_),
+            shortTokenAddress_
+        );
+        assertEq(
+            leveragedTokenFactory.pair(shortTokenAddress_),
+            longTokenAddress_
+        );
     }
 
     function testReverts() public {
         // Expect revert
         vm.expectRevert(ILeveragedTokenFactory.ZeroAddress.selector);
-        factory.createLeveragedTokens(address(0), 123);
+        leveragedTokenFactory.createLeveragedTokens(address(0), 123);
         vm.expectRevert(ILeveragedTokenFactory.ZeroLeverage.selector);
-        factory.createLeveragedTokens(Tokens.UNI, 0);
+        leveragedTokenFactory.createLeveragedTokens(Tokens.UNI, 0);
         vm.expectRevert(ILeveragedTokenFactory.MaxLeverage.selector);
-        factory.createLeveragedTokens(Tokens.UNI, 1000001);
-        factory.createLeveragedTokens(Tokens.UNI, 123);
+        leveragedTokenFactory.createLeveragedTokens(Tokens.UNI, 1000001);
+        leveragedTokenFactory.createLeveragedTokens(Tokens.UNI, 123);
         vm.expectRevert(ILeveragedTokenFactory.TokenExists.selector);
-        factory.createLeveragedTokens(Tokens.UNI, 123);
+        leveragedTokenFactory.createLeveragedTokens(Tokens.UNI, 123);
     }
 }
