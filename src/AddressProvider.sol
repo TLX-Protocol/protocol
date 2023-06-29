d9fae5c64d3cc8fc9cfead1729f1c1443b11cbd9
chase-manning <chase@manning.dev>
2023-06-29 15:50:10 +0100

    add oracle to address provider

--- src/AddressProvider.sol
+++ src/AddressProvider.sol
@@ -1,12 +1,20 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.13;
 
+import {Ownable} from "openzeppelin-contracts/contracts/access/Ownable.sol";
+
 import {IAddressProvider} from "./interfaces/IAddressProvider.sol";
 
-contract AddressProvider is IAddressProvider {
+contract AddressProvider is IAddressProvider, Ownable {
     address public immutable override leveragedTokenFactory;
+    address public override oracle;
 
-    constructor(address leveragedTokenFactory_) {
+    constructor(address leveragedTokenFactory_, address oracle_) {
         leveragedTokenFactory = leveragedTokenFactory_;
+        oracle = oracle_;
+    }
+
+    function setOracle(address oracle_) external onlyOwner {
+        oracle = oracle_;
     }
 }
