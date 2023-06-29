12b975841ec8d36d3e3e0d64f0f5269ed436aefc
chase-manning <chase@manning.dev>
2023-06-29 15:50:27 +0100

    add oracle to address provider interface

--- src/interfaces/IAddressProvider.sol
+++ src/interfaces/IAddressProvider.sol
@@ -3,4 +3,6 @@ pragma solidity ^0.8.13;
 
 interface IAddressProvider {
     function leveragedTokenFactory() external view returns (address);
+
+    function oracle() external view returns (address);
 }
