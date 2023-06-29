69925b5cd4ca18c5674ee6a52c5b2b34bba036a8
chase-manning <chase@manning.dev>
2023-06-29 14:45:00 +0100

    add wbtc to tokens library

--- src/libraries/Tokens.sol
+++ src/libraries/Tokens.sol
@@ -5,4 +5,5 @@ library Tokens {
     address public constant UNI = 0xFa7F8980b0f1E64A2062791cc3b0871572f1F7f0;
     address public constant USDC = 0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8;
     address public constant GMX = 0xfc5A1A6EB076a2C7aD06eD22C90d7E710E35ad0a;
+    address public constant WBTC = 0x2f2a2543B76A4166549F7aaB2e75Bef0aefC5B0f;
 }
