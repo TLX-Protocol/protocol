143c41d8c7670e82833a941928f4248833e72b8b
chase-manning <chase@manning.dev>
2023-06-29 12:21:41 +0100

    remove redundant import

--- test/LeveragedTokenFactory.sol
+++ test/LeveragedTokenFactory.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.13;
 
-import {Test} from "forge-std/Test.sol";
 import {IntegrationTest} from "./shared/IntegrationTest.sol";
 
 import {LeveragedTokenFactory} from "../src/LeveragedTokenFactory.sol";
