cf912b90638c2752917f0312e56aae05ff7b042d
chase-manning <chase@manning.dev>
2023-06-29 14:45:11 +0100

    create scaled number library

---
+++ src/libraries/ScaledNumber.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.13;
+
+library ScaledNumber {
+    uint8 internal constant _DEFAULT_DECIMALS = 18;
+
+    function scaleFrom(
+        uint256 value,
+        uint8 decimals
+    ) internal pure returns (uint256) {
+        if (decimals == _DEFAULT_DECIMALS) return value;
+
+        if (decimals > _DEFAULT_DECIMALS) {
+            unchecked {
+                return value / 10 ** (decimals - _DEFAULT_DECIMALS);
+            }
+        }
+
+        unchecked {
+            return value * 10 ** (_DEFAULT_DECIMALS - decimals);
+        }
+    }
+
+    function scaleTo(
+        uint256 value,
+        uint8 decimals
+    ) internal pure returns (uint256) {
+        if (decimals == _DEFAULT_DECIMALS) return value;
+
+        if (decimals > _DEFAULT_DECIMALS) {
+            unchecked {
+                return value * 10 ** (decimals - _DEFAULT_DECIMALS);
+            }
+        }
+
+        unchecked {
+            return value / 10 ** (_DEFAULT_DECIMALS - decimals);
+        }
+    }
+}
