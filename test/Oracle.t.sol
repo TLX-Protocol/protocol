99a1e9ee9ea2e4902b88e6e1417ecad9a987f097
chase-manning <chase@manning.dev>
2023-06-29 14:45:30 +0100

    test oracle contract

---
+++ test/Oracle.t.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.13;
+
+import {IntegrationTest} from "./shared/IntegrationTest.sol";
+
+import {Tokens} from "../src/libraries/Tokens.sol";
+import {Contracts} from "../src/libraries/Contracts.sol";
+
+import {Oracle} from "../src/Oracle.sol";
+import {IOracle} from "../src/interfaces/IOracle.sol";
+
+contract OracleTest is IntegrationTest {
+    Oracle public oracle;
+
+    function setUp() public {
+        oracle = new Oracle(Contracts.ETH_USD_ORACLE);
+        oracle.setUsdOracle(Tokens.UNI, Contracts.UNI_USD_ORACLE);
+    }
+
+    function testPriceThroughUsd() public {
+        uint256 price_ = oracle.getUsdPrice(Tokens.UNI);
+        assertGt(price_, 1e18);
+        assertLt(price_, 100e18);
+    }
+
+    function testPriceThroughEth() public {
+        oracle.setEthOracle(Tokens.WBTC, Contracts.WBTC_ETH_ORACLE);
+        uint256 price_ = oracle.getUsdPrice(Tokens.WBTC);
+        assertGt(price_, 5000e18);
+        assertLt(price_, 100000e18);
+    }
+
+    function testUpdateStalePriceDelay() public {
+        assertEq(oracle.stalePriceDelay(), 1 days);
+        oracle.setStalePriceDelay(0);
+        assertEq(oracle.stalePriceDelay(), 0 days);
+        vm.expectRevert(IOracle.StalePrice.selector);
+        oracle.getUsdPrice(Tokens.UNI);
+    }
+}
