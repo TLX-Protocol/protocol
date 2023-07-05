27962d81cc071442849795f293ba96f5bd0ffd55
chase-manning <chase@manning.dev>
2023-07-05 09:21:32 +0100

    add tests for position manager factory

---
+++ test/PositionManagerFactory.t.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.13;
+
+import {IntegrationTest} from "./shared/IntegrationTest.sol";
+
+import {Tokens} from "../src/libraries/Tokens.sol";
+
+import {IPositionManagerFactory} from "../src/interfaces/IPositionManagerFactory.sol";
+
+contract PositionManagerFactoryTest is IntegrationTest {
+    function testInit() public {
+        address[] memory positionManagers_ = positionManagerFactory
+            .positionManagers();
+        assertEq(positionManagers_.length, 0);
+        assertEq(
+            positionManagerFactory.positionManager(Tokens.UNI),
+            address(0)
+        );
+    }
+
+    function testCreatePositionManager() public {
+        positionManagerFactory.createPositionManager(Tokens.UNI);
+        address[] memory positionManagers_ = positionManagerFactory
+            .positionManagers();
+        assertEq(positionManagers_.length, 1);
+        assertEq(
+            positionManagerFactory.positionManager(Tokens.UNI),
+            positionManagers_[0]
+        );
+    }
+
+    function testRevertsWhenAlreadyExists() public {
+        positionManagerFactory.createPositionManager(Tokens.UNI);
+        vm.expectRevert(IPositionManagerFactory.AlreadyExists.selector);
+        positionManagerFactory.createPositionManager(Tokens.UNI);
+    }
+}
