9a5c198d8454fe5cc8f17f01ed4b32c7c6878ba3
chase-manning <chase@manning.dev>
2023-07-06 13:26:23 +0100

    add usdc usd oracle contract

--- src/libraries/Contracts.sol
+++ src/libraries/Contracts.sol
@@ -39,4 +39,6 @@ library Contracts {
         0x639Fe6ab55C921f74e7fac1ee960C0B6293ba612;
     address public constant WBTC_ETH_ORACLE =
         0xc5a90A6d7e4Af242dA238FFe279e9f2BA0c64B2e;
+    address public constant USDC_USD_ORACLE =
+        0x50834F3163758fcC1Df9973b6e91f0F0F0434aD3;
 }
