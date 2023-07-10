a83defb588205f490fcd61b61d24cb42e90ff051
chase-manning <chase@manning.dev>
2023-07-10 10:14:41 +0100

    remove redundant interface

--- src/interfaces/IDerivativesHandler.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: GPL-3.0
-pragma solidity ^0.8.13;
-
-interface IDerivativesHandler {
-    // To be called using delegatecall.
-    // Initializes the caller for trading derivatives via the DerivativesHandler.
-    function initialize() external;
-}
