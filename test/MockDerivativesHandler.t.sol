6adfaa87bf515607f8111fb49dbbbf5d0f4cf266
chase-manning <chase@manning.dev>
2023-07-24 14:54:09 +0100

    test mock derivatives handler

---
+++ test/MockDerivativesHandler.t.sol
@@ -0,0 +1,77 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.13;
+
+import {IERC20} from "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+
+import {IntegrationTest} from "./shared/IntegrationTest.sol";
+
+import {AddressKeys} from "../src/libraries/AddressKeys.sol";
+
+import {ILeveragedTokenFactory} from "../src/interfaces/ILeveragedTokenFactory.sol";
+import {Tokens} from "../src/libraries/Tokens.sol";
+import {ILeveragedToken} from "../src/interfaces/ILeveragedToken.sol";
+import {IDerivativesHandler} from "../src/interfaces/IDerivativesHandler.sol";
+
+contract MockDerivativesHandlerTest is IntegrationTest {
+    uint256 internal constant _AMOUNT = 100_000e6;
+
+    function setUp() public {
+        addressProvider.updateAddress(AddressKeys.ORACLE, address(mockOracle));
+        _mintTokensFor(Tokens.USDC, address(this), _AMOUNT);
+        IERC20(Tokens.USDC).approve(
+            address(mockDerivativesHandler),
+            type(uint256).max
+        );
+    }
+
+    function testInit() public {
+        assertEq(mockDerivativesHandler.hasPosition(), false);
+        vm.expectRevert(IDerivativesHandler.NoPositionExists.selector);
+        mockDerivativesHandler.closePosition();
+    }
+
+    function testCreatePosition() public {
+        mockDerivativesHandler.createPosition(
+            Tokens.USDC,
+            Tokens.UNI,
+            _AMOUNT,
+            2e18,
+            true
+        );
+        assertEq(mockDerivativesHandler.hasPosition(), true, "hasPosition");
+        IDerivativesHandler.Position memory position_ = mockDerivativesHandler
+            .position();
+        assertEq(position_.baseToken, Tokens.USDC);
+        assertEq(position_.targetToken, Tokens.UNI);
+        assertEq(position_.baseAmount, _AMOUNT);
+        assertEq(position_.leverage, 2e18);
+        assertEq(position_.isLong, true, "isLong");
+        assertEq(position_.hasProfit, false, "hasProfit");
+        assertEq(position_.delta, 0);
+    }
+
+    function testLongProfit() public {
+        mockDerivativesHandler.createPosition(
+            Tokens.USDC,
+            Tokens.UNI,
+            _AMOUNT,
+            2e18,
+            true
+        );
+        uint256 uniPrice_ = mockOracle.getUsdPrice(Tokens.UNI);
+        mockOracle.setPrice(Tokens.UNI, uniPrice_ * 2);
+        IDerivativesHandler.Position memory position_ = mockDerivativesHandler
+            .position();
+        assertEq(position_.baseAmount, _AMOUNT);
+        assertEq(position_.leverage, 2e18);
+        assertEq(position_.isLong, true, "isLong");
+        assertEq(position_.hasProfit, true, "hasProfit");
+        assertEq(position_.delta, _AMOUNT * 2);
+
+        uint256 owed_ = mockDerivativesHandler.closePosition();
+
+        assertEq(owed_, _AMOUNT * 3);
+        assertEq(mockDerivativesHandler.hasPosition(), false, "hasPosition");
+        assertEq(IERC20(Tokens.USDC).balanceOf(address(this)), _AMOUNT * 3);
+    }
+}
