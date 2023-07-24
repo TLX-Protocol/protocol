dad5ebeb60d6922fab890d11974a7fb4b96fa6a0
chase-manning <chase@manning.dev>
2023-07-24 14:53:41 +0100

    add derivatives handler interface

---
+++ src/interfaces/IDerivativesHandler.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.13;
+
+interface IDerivativesHandler {
+    struct Position {
+        uint256 createdAt;
+        address baseToken;
+        address targetToken;
+        uint256 baseAmount;
+        uint256 leverage;
+        bool isLong;
+        bool hasProfit;
+        uint256 delta;
+    }
+
+    error NoPositionExists();
+    error PositionAlreadyExists();
+
+    function createPosition(
+        address baseToken,
+        address targetToken,
+        uint256 baseAmount,
+        uint256 leverage,
+        bool isLong
+    ) external;
+
+    function closePosition() external returns (uint256);
+
+    function position() external view returns (Position memory);
+
+    function hasPosition() external view returns (bool);
+}
