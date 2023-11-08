55074367cd611f9f424ea52d5ecc58029213e3fc
Chase Manning <chase@manning.dev>
2023-11-08 13:27:09 +0000

    get better errors for delegate call

--- test/SynthetixHandler.t.sol
+++ test/SynthetixHandler.t.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.13;
 
 import {IERC20} from "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import {Address} from "openzeppelin-contracts/contracts/utils/Address.sol";
 
 import {IntegrationTest} from "./shared/IntegrationTest.sol";
 
@@ -12,6 +13,8 @@ import {Symbols} from "../src/libraries/Symbols.sol";
 import {Tokens} from "../src/libraries/Tokens.sol";
 
 contract SynthetixHandlerTest is IntegrationTest {
+    using Address for address;
+
     SynthetixHandler public synthetixHandler;
 
     function setUp() public {
@@ -36,6 +39,12 @@ contract SynthetixHandlerTest is IntegrationTest {
         _withdrawMargin(50e18);
         uint256 balanceAfter = IERC20(Tokens.SUSD).balanceOf(address(this));
         assertEq(balanceAfter - balanceBefore, 50e18);
+    }
+
+    function testSubmitLeverageUpdate() public {
+        _mintTokensFor(Tokens.SUSD, address(this), 100e18);
+        _depositMargin(100e18);
+        _submitLeverageUpdate(2e18, true);
     }
 
     function testHasOpenPosition() public {
@@ -64,24 +73,33 @@ contract SynthetixHandlerTest is IntegrationTest {
     }
 
     function _depositMargin(uint256 amount_) internal {
-        (bool success, ) = address(synthetixHandler).delegatecall(
+        address(synthetixHandler).functionDelegateCall(
             abi.encodeWithSignature(
                 "depositMargin(string,uint256)",
                 Symbols.ETH,
                 amount_
             )
         );
-        assertTrue(success);
     }
 
     function _withdrawMargin(uint256 amount_) internal {
-        (bool success, ) = address(synthetixHandler).delegatecall(
+        address(synthetixHandler).functionDelegateCall(
             abi.encodeWithSignature(
                 "withdrawMargin(string,uint256)",
                 Symbols.ETH,
                 amount_
             )
         );
-        assertTrue(success);
+    }
+
+    function _submitLeverageUpdate(uint256 leverage_, bool isLong_) internal {
+        address(synthetixHandler).functionDelegateCall(
+            abi.encodeWithSignature(
+                "submitLeverageUpdate(string,uint256,bool)",
+                Symbols.ETH,
+                leverage_,
+                isLong_
+            )
+        );
     }
 }
