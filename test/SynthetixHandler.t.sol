85b4ee1c4713d027facc44a11bb92204a1f2c5a4
Chase Manning <chase@manning.dev>
2023-11-16 14:57:19 +0000

    refactor to use integration tests for executing

--- test/SynthetixHandler.t.sol
+++ test/SynthetixHandler.t.sol
@@ -3,36 +3,18 @@ pragma solidity ^0.8.13;
 
 import {IERC20} from "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 import {Address} from "openzeppelin-contracts/contracts/utils/Address.sol";
-import {Strings} from "openzeppelin-contracts/contracts/utils/Strings.sol";
-
-import {IPerpsV2MarketData} from "../src/interfaces/synthetix/IPerpsV2MarketData.sol";
-import {IPerpsV2MarketConsolidated} from "../src/interfaces/synthetix/IPerpsV2MarketConsolidated.sol";
 
 import {IntegrationTest} from "./shared/IntegrationTest.sol";
 
-import {Contracts} from "../src/libraries/Contracts.sol";
 import {Symbols} from "../src/libraries/Symbols.sol";
 import {Tokens} from "../src/libraries/Tokens.sol";
 import {ScaledNumber} from "../src/libraries/ScaledNumber.sol";
 
-import {Base64} from "../src/testing/Base64.sol";
-
 import "forge-std/console.sol";
-import "forge-std/StdJson.sol";
 
 contract SynthetixHandlerTest is IntegrationTest {
     using Address for address;
-    using stdJson for string;
     using ScaledNumber for uint256;
-
-    string constant PYTH_URL = "https://xc-mainnet.pyth.network/api/get_vaa";
-    string constant PYTH_ID =
-        "0xff61491a931112ddf1bd8147cd1b641375f79f5825126d665480874634fd0ace"; // ETH/USD
-
-    struct PythResponse {
-        string vaa;
-        uint256 publishTime;
-    }
 
     receive() external payable {}
 
@@ -206,49 +188,5 @@ contract SynthetixHandlerTest is IntegrationTest {
         address(synthetixHandler).functionDelegateCall(
             abi.encodeWithSignature("cancelLeverageUpdate(string)", Symbols.ETH)
         );
-    }
-
-    function _executeOrder() internal {
-        uint256 currentTime = block.timestamp;
-        uint256 searchTime = currentTime + 5;
-        string memory vaa = _getVaa(searchTime);
-        bytes memory decoded = Base64.decode(vaa);
-        bytes memory hexData = abi.encodePacked(decoded);
-        bytes[] memory priceUpdateData = new bytes[](1);
-        priceUpdateData[0] = hexData;
-        _market(Symbols.ETH).executeOffchainDelayedOrder{value: 1 ether}(
-            address(this),
-            priceUpdateData
-        );
-    }
-
-    function _getVaa(uint256 publishTime) internal returns (string memory) {
-        string memory url = string.concat(
-            PYTH_URL,
-            "?id=",
-            PYTH_ID,
-            "&publish_time=",
-            Strings.toString(publishTime)
-        );
-        string[] memory inputs = new string[](3);
-        inputs[0] = "curl";
-        inputs[1] = url;
-        inputs[2] = "-s";
-        bytes memory res = vm.ffi(inputs);
-        return abi.decode(string(res).parseRaw(".vaa"), (string));
-    }
-
-    function _market(
-        string memory targetAsset_
-    ) internal view returns (IPerpsV2MarketConsolidated) {
-        IPerpsV2MarketData.MarketData memory marketData_ = IPerpsV2MarketData(
-            Contracts.PERPS_V2_MARKET_DATA
-        ).marketDetailsForKey(_key(targetAsset_));
-        require(marketData_.market != address(0), "No market");
-        return IPerpsV2MarketConsolidated(marketData_.market);
-    }
-
-    function _key(string memory targetAsset_) internal pure returns (bytes32) {
-        return bytes32(bytes(abi.encodePacked("s", targetAsset_, "PERP")));
     }
 }
