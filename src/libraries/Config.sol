096e948e4af8de247ce525871abce7d17b5bc5db
Chase Manning <chase@manning.dev>
2023-12-13 09:51:46 +0000

    several improvements to automation

--- src/libraries/Config.sol
+++ src/libraries/Config.sol
@@ -22,4 +22,5 @@ library Config {
     uint256 public constant EARNINGS_PERCENT = 0.5e18; // 50%
     uint256 public constant REBALANCE_THRESHOLD = 0.25e18; // 25%
     uint256 public constant STREAMING_FEE = 0.02e18; // 2%
+    uint256 public constant MAX_REBALANCES = 20; // The maximum number of rebalances that can be performed in a single transaction
 }
