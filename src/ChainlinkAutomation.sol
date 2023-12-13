126655fca1e59adafc6059a12dc3e42182a372b5
Chase Manning <chase@manning.dev>
2023-12-13 09:51:46 +0000

    several improvements to automation

--- src/ChainlinkAutomation.sol
+++ src/ChainlinkAutomation.sol
@@ -2,22 +2,23 @@
 pragma solidity ^0.8.13;
 
 import {AutomationCompatibleInterface} from "chainlink/src/v0.8/automation/AutomationCompatible.sol";
+import {Ownable} from "openzeppelin-contracts/contracts/access/Ownable.sol";
 
 import {Errors} from "./libraries/Errors.sol";
 
 import {ILeveragedToken} from "./interfaces/ILeveragedToken.sol";
 import {IAddressProvider} from "./interfaces/IAddressProvider.sol";
 
-contract ChainlinkAutomation is AutomationCompatibleInterface {
-    uint256 internal constant MAX_REBALANCES = 20;
+contract ChainlinkAutomation is AutomationCompatibleInterface, Ownable {
+    error NoRebalancableTokens();
+
+    uint256 internal immutable MAX_REBALANCES;
 
     IAddressProvider internal immutable _addressProvider;
 
-    address internal immutable _upkeep;
-
-    constructor(address addressProvider_) {
+    constructor(address addressProvider_, uint256 maxReblances_) {
         _addressProvider = IAddressProvider(addressProvider_);
-        _upkeep = msg.sender; // TODO Change this to the actual upkeep contract
+        MAX_REBALANCES = maxReblances_;
     }
 
     function checkUpkeep(
@@ -28,30 +29,28 @@ contract ChainlinkAutomation is AutomationCompatibleInterface {
             .allTokens();
 
         address[] memory rebalancableTokens_ = new address[](MAX_REBALANCES);
-        uint256 rebalancableTokensCount_ = 0;
-        for (uint256 i = 0; i < tokens_.length; i++) {
+        uint256 rebalancableTokensCount_;
+        for (uint256 i; i < tokens_.length; i++) {
             if (!ILeveragedToken(tokens_[i]).canRebalance()) continue;
-            upkeepNeeded = true;
             rebalancableTokens_[rebalancableTokensCount_] = tokens_[i];
             rebalancableTokensCount_++;
             if (rebalancableTokensCount_ == MAX_REBALANCES) break;
         }
 
-        if (!upkeepNeeded) return (false, "");
+        if (rebalancableTokensCount_ == 0) return (false, "");
+        upkeepNeeded = true;
         performData = abi.encode(rebalancableTokensCount_, rebalancableTokens_);
     }
 
-    function performUpkeep(bytes calldata performData) external {
-        if (msg.sender != _upkeep) revert Errors.NotAuthorized();
-
+    function performUpkeep(bytes calldata performData) external onlyOwner {
         (
             uint256 rebalancableTokensCount_,
             address[] memory rebalancableTokens_
         ) = abi.decode(performData, (uint256, address[]));
 
-        if (rebalancableTokensCount_ == 0) return;
+        if (rebalancableTokensCount_ == 0) revert NoRebalancableTokens();
 
-        for (uint256 i = 0; i < rebalancableTokensCount_; i++) {
+        for (uint256 i; i < rebalancableTokensCount_; i++) {
             ILeveragedToken(rebalancableTokens_[i]).rebalance();
         }
     }
--- test/ChainlinkAutomation.t.sol
+++ test/ChainlinkAutomation.t.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.13;
 import {IntegrationTest} from "./shared/IntegrationTest.sol";
 
 import {Symbols} from "../src/libraries/Symbols.sol";
+import {Config} from "../src/libraries/Config.sol";
 
 import {ChainlinkAutomation} from "../src/ChainlinkAutomation.sol";
 
@@ -12,7 +13,10 @@ contract ChainlinkAutomationTest is IntegrationTest {
 
     function setUp() public override {
         super.setUp();
-        chainlinkAutomation = new ChainlinkAutomation(address(addressProvider));
+        chainlinkAutomation = new ChainlinkAutomation(
+            address(addressProvider),
+            Config.MAX_REBALANCES
+        );
     }
 
     function testInit() public {
