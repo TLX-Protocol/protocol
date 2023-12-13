b8de3584235458fcc2cb4e9b1199aac627f934fe
Chase Manning <chase@manning.dev>
2023-12-13 09:56:03 +0000

    fix lint errors

--- src/ChainlinkAutomation.sol
+++ src/ChainlinkAutomation.sol
@@ -10,36 +10,15 @@ import {ILeveragedToken} from "./interfaces/ILeveragedToken.sol";
 import {IAddressProvider} from "./interfaces/IAddressProvider.sol";
 
 contract ChainlinkAutomation is AutomationCompatibleInterface, Ownable {
-    error NoRebalancableTokens();
-
-    uint256 internal immutable MAX_REBALANCES;
+    uint256 internal immutable _maxRebalances;
 
     IAddressProvider internal immutable _addressProvider;
 
+    error NoRebalancableTokens();
+
     constructor(address addressProvider_, uint256 maxReblances_) {
         _addressProvider = IAddressProvider(addressProvider_);
-        MAX_REBALANCES = maxReblances_;
-    }
-
-    function checkUpkeep(
-        bytes calldata
-    ) external view returns (bool upkeepNeeded, bytes memory performData) {
-        address[] memory tokens_ = _addressProvider
-            .leveragedTokenFactory()
-            .allTokens();
-
-        address[] memory rebalancableTokens_ = new address[](MAX_REBALANCES);
-        uint256 rebalancableTokensCount_;
-        for (uint256 i; i < tokens_.length; i++) {
-            if (!ILeveragedToken(tokens_[i]).canRebalance()) continue;
-            rebalancableTokens_[rebalancableTokensCount_] = tokens_[i];
-            rebalancableTokensCount_++;
-            if (rebalancableTokensCount_ == MAX_REBALANCES) break;
-        }
-
-        if (rebalancableTokensCount_ == 0) return (false, "");
-        upkeepNeeded = true;
-        performData = abi.encode(rebalancableTokensCount_, rebalancableTokens_);
+        _maxRebalances = maxReblances_;
     }
 
     function performUpkeep(bytes calldata performData) external onlyOwner {
@@ -53,5 +32,26 @@ contract ChainlinkAutomation is AutomationCompatibleInterface, Ownable {
         for (uint256 i; i < rebalancableTokensCount_; i++) {
             ILeveragedToken(rebalancableTokens_[i]).rebalance();
         }
+    }
+
+    function checkUpkeep(
+        bytes calldata
+    ) external view returns (bool upkeepNeeded, bytes memory performData) {
+        address[] memory tokens_ = _addressProvider
+            .leveragedTokenFactory()
+            .allTokens();
+
+        address[] memory rebalancableTokens_ = new address[](_maxRebalances);
+        uint256 rebalancableTokensCount_;
+        for (uint256 i; i < tokens_.length; i++) {
+            if (!ILeveragedToken(tokens_[i]).canRebalance()) continue;
+            rebalancableTokens_[rebalancableTokensCount_] = tokens_[i];
+            rebalancableTokensCount_++;
+            if (rebalancableTokensCount_ == _maxRebalances) break;
+        }
+
+        if (rebalancableTokensCount_ == 0) return (false, "");
+        upkeepNeeded = true;
+        performData = abi.encode(rebalancableTokensCount_, rebalancableTokens_);
     }
 }
