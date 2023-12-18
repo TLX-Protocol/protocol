d6c9e39f50ca622aca275f2d40c7407d60aad545
Chase Manning <chase@manning.dev>
2023-12-18 16:11:28 +0000

    add token minting to deployment script

--- script/deployments/shared/DeploymentScript.s.sol
+++ script/deployments/shared/DeploymentScript.s.sol
@@ -5,11 +5,17 @@ import "forge-std/Script.sol";
 import "forge-std/console.sol";
 import "forge-std/StdJson.sol";
 
+import {stdStorage, StdStorage} from "forge-std/Test.sol";
+
+import {IERC20} from "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+
 import {Strings} from "openzeppelin-contracts/contracts/utils/Strings.sol";
 
 import {Config} from "../../../src/libraries/Config.sol";
+import {Tokens} from "../../../src/libraries/Tokens.sol";
 
 abstract contract DeploymentScript is Script {
+    using stdStorage for StdStorage;
     using stdJson for string;
 
     string public constant DEPLOYMENTS_PATH = "deployments.json";
@@ -44,5 +50,22 @@ abstract contract DeploymentScript is Script {
         string memory json = vm.readFile(DEPLOYMENTS_PATH);
         string memory key = string.concat(".", name);
         return json.readAddress(key);
+    }
+
+    function _mintTokensFor(
+        address token_,
+        address account_,
+        uint256 amount_
+    ) internal {
+        // sUSD is weird, this is a workaround to fix minting for it.
+        if (token_ == Tokens.SUSD) {
+            token_ = 0x92bAc115d89cA17fd02Ed9357CEcA32842ACB4c2;
+        }
+
+        stdstore
+            .target(token_)
+            .sig(IERC20(token_).balanceOf.selector)
+            .with_key(account_)
+            .checked_write(amount_);
     }
 }
