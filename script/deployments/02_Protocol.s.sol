ba40ac9a476e339aa5138adf17ed226926057217
Chase Manning <chase@manning.dev>
2023-12-18 16:11:37 +0000

    add tests for protocol deployment

--- script/deployments/02_Protocol.s.sol
+++ script/deployments/02_Protocol.s.sol
@@ -1,22 +1,31 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.13;
 
+import {Test} from "forge-std/Test.sol";
+
+import {IERC20} from "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+
 import {DeploymentScript} from "./shared/DeploymentScript.s.sol";
 
 import {Contracts} from "../../src/libraries/Contracts.sol";
 import {AddressKeys} from "../../src/libraries/AddressKeys.sol";
 import {Config} from "../../src/libraries/Config.sol";
 import {LeveragedTokens} from "../../src/libraries/LeveragedTokens.sol";
+import {Vestings} from "../../src/libraries/Vestings.sol";
+import {Config} from "../../src/libraries/Config.sol";
 
 import {IAddressProvider} from "../../src/interfaces/IAddressProvider.sol";
 import {IBonding} from "../../src/interfaces/IBonding.sol";
+import {ILeveragedToken} from "../../src/interfaces/ILeveragedToken.sol";
 
 import {LeveragedTokenFactory} from "../../src/LeveragedTokenFactory.sol";
 import {Referrals} from "../../src/Referrals.sol";
 import {SynthetixHandler} from "../../src/SynthetixHandler.sol";
 import {ChainlinkAutomation} from "../../src/ChainlinkAutomation.sol";
 
-contract ProtocolDeployment is DeploymentScript {
+import {IAddressProvider} from "../../src/interfaces/IAddressProvider.sol";
+
+contract ProtocolDeployment is DeploymentScript, Test {
     function _run() internal override {
         // Getting deployed contracts
         IAddressProvider addressProvider = IAddressProvider(
@@ -85,5 +94,28 @@ contract ProtocolDeployment is DeploymentScript {
 
         // Launching bonding
         bonding.launch();
+    }
+
+    function testProtocolDeployment() public {
+        IAddressProvider addressProvider = IAddressProvider(
+            _getDeployedAddress("AddressProvider")
+        );
+        ILeveragedToken leveragedToken = ILeveragedToken(
+            addressProvider.leveragedTokenFactory().allTokens()[0]
+        );
+        uint256 baseAssetAmmount = 100_000e18;
+        _mintTokensFor(Config.REWARD_TOKEN, address(this), baseAssetAmmount);
+        IERC20(Config.REWARD_TOKEN).approve(
+            address(leveragedToken),
+            baseAssetAmmount
+        );
+        assertEq(leveragedToken.totalSupply(), 0);
+        leveragedToken.mint(baseAssetAmmount, 0);
+        assertGt(leveragedToken.totalSupply(), 0);
+        skip(1 hours);
+        assertApproxEqRel(leveragedToken.exchangeRate(), 1e18, 0.1e18);
+        assertGt(addressProvider.bonding().availableTlx(), 1e18);
+        leveragedToken.approve(address(addressProvider.bonding()), 50_000e18);
+        addressProvider.bonding().bond(address(leveragedToken), 50_000e18, 1);
     }
 }
