6d9d941dcbeab0bcdf60a6a1ae829769433c3e16
Chase Manning <chase@manning.dev>
2023-12-22 09:43:13 +0000

    update placeholder address to not zero address

--- src/libraries/Config.sol
+++ src/libraries/Config.sol
@@ -11,7 +11,7 @@ library Config {
     address public constant TREASURY = address(1); // Our multisig
     address public constant POL = TREASURY; // Where the bonding tokens ar sent to
     address public constant REBALANCE_FEE_RECEIVER = address(2); // Receiver of rebalance fees (may be treasury to start)
-    address public constant CHAINLINK_AUTOMATION_FORWARDER_ADDRESS = address(0); // The forwarder address for Chainlink automation
+    address public constant CHAINLINK_AUTOMATION_FORWARDER_ADDRESS = address(7); // The forwarder address for Chainlink automation
 
     // Strings
     string public constant TOKEN_NAME = "TLX DAO Token"; // TLX DAO Token
