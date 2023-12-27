1dad0efd7de6931d6c971265d9b124fa9e23dcfd
Chase Manning <chase@manning.dev>
2023-12-27 11:24:47 +0000

    fix formatting issue with contracts.sol

--- src/libraries/Contracts.sol
+++ src/libraries/Contracts.sol
@@ -7,6 +7,6 @@ library Contracts {
         0x340B5d664834113735730Ad4aFb3760219Ad9112;
     address public constant VELODROME_ROUTER =
         0xa062aE8A9c5e11aaA026fc2670B0D65cCc8B2858;
-    address public constant VELODROME_DEFAULT_FACTORY = 
+    address public constant VELODROME_DEFAULT_FACTORY =
         0xF1046053aa5682b4F9a81b5481394DA16BE5FF5a;
 }
