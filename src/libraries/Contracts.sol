b788e09b503bd0aa1829f36a91dafd31ce9054ea
Chase Manning <chase@manning.dev>
2024-02-28 16:31:11 +0000

    add more synthetix contracts

--- src/libraries/Contracts.sol
+++ src/libraries/Contracts.sol
@@ -7,6 +7,10 @@ library Contracts {
         0x340B5d664834113735730Ad4aFb3760219Ad9112;
     address public constant PERPS_V2_MARKET_SETTINGS =
         0x649F44CAC3276557D03223Dbf6395Af65b11c11c;
+    address public constant EXCHANGE_RATES =
+        0x913bd76F7E1572CC8278CeF2D6b06e2140ca9Ce2;
+    address public constant PERPS_V2_EXCHANGE_RATE =
+        0x2C15259D4886e2C0946f9aB7a5E389c86b3c3b04;
 
     address public constant VELODROME_ROUTER =
         0xa062aE8A9c5e11aaA026fc2670B0D65cCc8B2858;
