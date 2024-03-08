c870cf305ab78f23458e202f6ceeb593fc319dd1
Chase Manning <chase@manning.dev>
2024-03-08 16:49:22 +0000

    add zero validation for base for all tlx

--- src/Bonding.sol
+++ src/Bonding.sol
@@ -103,6 +103,7 @@ contract Bonding is IBonding, TlxOwnable {
         uint256 baseForAllTlx_
     ) external override onlyOwner {
         if (!isLive) revert BondingNotLive();
+        if (baseForAllTlx_ == 0) revert Errors.InsufficientAmount();
         _updateCache();
         _baseForAllTlx = baseForAllTlx_;
     }
