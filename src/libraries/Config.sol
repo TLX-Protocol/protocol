713056e23218acf9880d191b6b11b488c5d4196c
Chase Manning <chase@manning.dev>
2024-03-08 17:27:31 +0000

    remove redundant max leverage variable

--- src/libraries/Config.sol
+++ src/libraries/Config.sol
@@ -42,7 +42,6 @@ library Config {
     uint256 public constant VESTING_DURATION = 365 days; // 1 year
     uint256 public constant REBALANCE_FEE = 5e18; // 5 sUSD
     uint256 public constant REBALANCE_BASE_NEXT_ATTEMPT_DELAY = 1 minutes; // 1 minute (doubles each attempt)
-    uint256 public constant MAX_LEVERAGE = 50e18; // 50x
     uint256 public constant REDEMPTION_FEE = 0.005e18; // 0.5%
     uint256 public constant REBATE_PERCENT = 0.5e18; // 50%
     uint256 public constant EARNINGS_PERCENT = 0.5e18; // 50%
